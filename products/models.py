b5a9dd7a6bd0eecde89999356bde6a81f5c51875
AndrewHyland757 <hyland757@gmail.com>
2024-03-20 00:41:41 +0000

    Add admin.py file to register the models and add prepopulated fileds

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     description = models.TextField(blank=False)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='products/')
 
 
     class Meta:
